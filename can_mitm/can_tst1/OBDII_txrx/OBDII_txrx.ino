15135efc85324ba3bd6df02a404c905621eadd62
carmaster01 <36508414+carmaster01@users.noreply.github.com>
2018-03-27 12:49:41 -0400

    Add files via upload

---
+++ can_mitm/can_tst1/OBDII_txrx/OBDII_txrx.ino
@@ -0,0 +1,170 @@
+/************************************************************************************************* 
+#naeem
+  Query
+  send id: 0x7df
+      dta: 0x02, 0x01, PID_CODE, 0, 0, 0, 0, 0
+
+  Response
+  From id: 0x7E9 or 0x7EA or 0x7EB
+      dta: len, 0x41, PID_CODE, byte0, byte1(option), byte2(option), byte3(option), byte4(option)
+      
+  https://en.wikipedia.org/wiki/OBD-II_PIDs
+  
+***************************************************************************************************/
+#include <SPI.h>
+#include "mcp_can.h"
+
+// the cs pin of the version after v1.1 is default to D9
+// v0.9b and v1.0 is default D10
+const int SPI_CS_PIN_tx = 10;
+const int SPI_CS_PIN_rx = 9;
+
+MCP_CAN CAN(SPI_CS_PIN_tx);                                    // Set CS tx pin
+MCP_CAN CANRX(SPI_CS_PIN_rx);                                    // Set CS tx pin
+
+
+#define PID_ENGIN_PRM       0x0C
+#define PID_VEHICLE_SPEED   0x0D
+#define PID_COOLANT_TEMP    0x05
+
+#define CAN_ID_PID          0x7E8
+
+unsigned char PID_INPUT = PID_VEHICLE_SPEED;
+unsigned char getPid    = 0;
+String buf_str;
+
+void set_mask_filt()
+{
+    /*
+     * set mask, set both the mask to 0x3ff
+     */
+    CAN.init_Mask(0, 0, 0x7FC);
+    CAN.init_Mask(1, 0, 0x7FC);
+
+    /*
+     * set filter, we can receive id from 0x04 ~ 0x09
+     */
+    CAN.init_Filt(0, 0, 0x7E8);                 
+    CAN.init_Filt(1, 0, 0x7E8);
+
+    CAN.init_Filt(2, 0, 0x7E8);
+    CAN.init_Filt(3, 0, 0x7E8);
+    CAN.init_Filt(4, 0, 0x7E8); 
+    CAN.init_Filt(5, 0, 0x7E8);
+}
+
+void sendPid(unsigned char __pid)
+{
+    unsigned char tmp[8] = {0x02, 0x01, __pid, 0, 0, 0, 0, 0};
+    Serial.print("SEND PID: 0x");
+    Serial.println(__pid, HEX);
+    CAN.sendMsgBuf(CAN_ID_PID, 0, 8, tmp);
+}
+
+
+boolean taskCanRecv()
+{
+    unsigned char len = 0;
+    unsigned char buf[10];
+    buf_str = "";
+    boolean rxd = false;
+
+    if(CANRX.checkReceive())                   // check if get data
+    {
+        rxd = true;
+        CANRX.readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
+
+        //Serial.println("\r\n------------------------------------------------------------------");
+        //Serial.print("Get Data From id: ");
+        for(int i = 0; i<len; i++)    // print the data
+        {
+            Serial.print("0x");
+            Serial.print(buf[i], HEX);
+            Serial.print("\t");    
+        }
+        if (len>0)
+        {
+          Serial.println();
+        }
+    }
+
+    return rxd;
+}
+
+void taskDbg()
+{
+    while(Serial.available())
+    {
+        char c = Serial.read();
+        
+        if(c>='0' && c<='9')
+        {
+            PID_INPUT *= 0x10;
+            PID_INPUT += c-'0';
+            
+        }
+        else if(c>='A' && c<='F')
+        {
+            PID_INPUT *= 0x10;
+            PID_INPUT += 10+c-'A';
+        }
+        else if(c>='a' && c<='f')
+        {
+            PID_INPUT *= 0x10;
+            PID_INPUT += 10+c-'a';
+        }
+        else if(c == '\n')      // END
+        {
+            getPid = 1;
+        }
+    }
+}
+
+
+void setup()
+{
+    Serial.begin(115200);
+    while (CAN_OK != CAN.begin(CAN_500KBPS))    // init can bus : baudrate = 500k
+    {
+        Serial.println("CAN BUS Shield init fail");
+        Serial.println(" Init CAN BUS Shield again");
+        delay(100);
+    }
+    Serial.println("CAN BUS Shield init ok!");
+    //set_mask_filt();
+}
+
+
+void loop()
+{
+    //if (Serial.available())
+    //{
+      //char c = Serial.read();
+      //sendPid(c);
+    //delay(1);
+    //}
+   
+    
+    if (taskCanRecv())
+    {
+      Serial.print(buf_str);
+      buf_str="";
+      //sendPid("0");
+      delay(1);
+    }
+    
+
+    if(Serial.read() == 'y')
+    {
+      //Serial.println("yo");
+      sendPid("0");
+      delay(1);
+    }
+    //sendPid("0");
+    
+    taskDbg();
+    
+
+
+}
+// END FILE
---
+++ can_mitm/can_tst1/can_tst1.ino
@@ -0,0 +1,73 @@
+// naeems's Bench
+// 1st CAN Network - CAN TRANSMIT
+
+#include <mcp_can.h>
+#include <SPI.h>
+
+
+const int SPI_CS_PIN = 9;
+const int SPI_CS_PINrx = 10;
+
+// Build an ID or PGN
+
+long unsigned int txID = 0x1881ABBA; // This format is typical of a 29 bit identifier.. the most significant digit is never greater than one.
+//unsigned char stmp[8] = {0x0E, 0x00, 0xFF, 0x22, 0xE9, 0xFA, 0xDD, 0x51};
+unsigned char stmp[8] = {0xAA, 0xBB, 0xCC, 0xFF, 0xFF, 0xFF, 0xDD, 0xEE};
+
+unsigned char len = 0;
+unsigned char buf[8];
+byte a = 0;
+//Construct a MCP_CAN Object and set Chip Select to 10.
+
+MCP_CAN CAN(SPI_CS_PIN);                            
+MCP_CAN CANRX(SPI_CS_PINrx);                            
+
+void setup()
+{
+    Serial.begin(115200);
+
+    while (CAN_OK != CAN.begin(CAN_250KBPS))              // init can bus : baudrate = 250K
+    {
+         Serial.println("CAN BUS Module Failed to Initialized");
+        Serial.println("Retrying....");
+        delay(200);
+        
+    }
+    Serial.println("CAN BUS Shield init ok!");
+}
+
+
+void loop()
+{   //Serial.println("In loop");
+
+    // send the data:  id = 0x00, Extended Frame, data len = 8, stmp: data buf
+    // Extended Frame = 1.
+    
+    CAN.sendMsgBuf(txID,1, 8, stmp);    
+    delay(10);    // send data every 500mS
+    CANRX.readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
+    delay(10);    // send data every 500mS
+    //Serial.println(len);
+    for(int i = 0; i<len; i++)    // print the data
+    {
+        if(buf[i] > 15){
+          Serial.print("0x");
+          Serial.print(buf[i], HEX);    
+        }
+      else{
+          Serial.print("0x0");
+          Serial.print(buf[i], HEX);
+      }  
+        
+        //Serial.print("0x");
+        //Serial.print(buf[i], HEX);
+        
+        Serial.print("\t");            
+    }
+
+    if (len>0)
+    {
+      Serial.println("");
+    }
+    //Serial.println(buf);
+}
